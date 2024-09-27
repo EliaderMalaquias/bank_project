ab5bdf819f20dc3b38d7adce456c12ace130933e
EliaderMalaquias <joseeliader190@gmail.com>
2024-09-27 20:01:03 -0300

    Criando controller de user e funcionalidades

--- src/main/java/com/Inter/Online/service/UserService.java
+++ src/main/java/com/Inter/Online/service/UserService.java
@@ -5,11 +5,17 @@ import com.Inter.Online.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 
     public User saveUser(User user) {
         return userRepository.save(user);
