c343a207b7d804dd399991dfc73355917d3d55f8
EliaderMalaquias <joseeliader190@gmail.com>
2024-09-27 19:40:43 -0300

    Criando Service de User e funcionalidades

---
+++ src/main/java/com/Inter/Online/service/UserService.java
@@ -0,0 +1,17 @@
+package com.Inter.Online.service;
+
+import com.Inter.Online.model.User;
+import com.Inter.Online.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+}
